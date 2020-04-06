a0bf7f1f2ebfc7aba04be8830ed643b7f0b9e2c0
Jagannath Bhat <jagannathbhatjb@gmail.com>
2020-04-06 18:03:20 +0530

    add build folder

---
+++ client/build/precache-manifest.71165ffa580e7e8e3d57b59391fa1a5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce77e7cf369f4df29569d56e747f883",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4616be0c6c9475931ccc",
+    "url": "/static/css/main.79cd23bb.chunk.css"
+  },
+  {
+    "revision": "18e110e17b181717fcaa",
+    "url": "/static/js/2.2cdf58ec.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2cdf58ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4616be0c6c9475931ccc",
+    "url": "/static/js/main.62c317c6.chunk.js"
+  },
+  {
+    "revision": "148ae24cc6f15b610e14",
+    "url": "/static/js/runtime-main.828f75aa.js"
+  },
+  {
+    "revision": "2bace9e0f264039bbde0ac41073c6f86",
+    "url": "/static/media/header.2bace9e0.svg"
+  }
+]);
